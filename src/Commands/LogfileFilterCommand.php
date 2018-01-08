6f4446907bef8a8e7882cc29af264938405b1be0
Austin Dean <amberdean89@gmail.com>
2018-01-08 07:15:19 -0500

    Adjusted license text in file headings.

--- src/Commands/LogfileFilterCommand.php
+++ src/Commands/LogfileFilterCommand.php
@@ -5,7 +5,7 @@
  * (c) 2018 Austin Dean
  *
  * For the full copyright and license information, please view
- * the license that is located at the bottom of this file.
+ * the license that was distributed with the source code.
  */
 
 namespace ATDean\HttpdLogSlice\Commands;

5b0f695d5e144b370bdd56f9d0be624be2087abc
Austin Dean <amberdean89@gmail.com>
2018-01-08 05:48:47 -0500

    Argument handler set up on LogfileFilterCommand.

--- src/Commands/LogfileFilterCommand.php
+++ src/Commands/LogfileFilterCommand.php
@@ -11,6 +11,7 @@
 namespace ATDean\HttpdLogSlice\Commands;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -23,11 +24,27 @@ class LogfileFilterCommand extends Command
     {
         $this->setName('logfile:filter')
              ->setDescription('Parses the specified logfile.')
-             ->setHelp('Parses the specified logfile and returns an analysis and/or subset of the data as specified by the given arguments.');
+             ->setHelp('Parses the specified logfile and returns an analysis '
+                 . 'and/or subset of the data as specified by the given arguments.')
+             ->addArgument(
+                 'filepaths',
+                 InputArgument::IS_ARRAY | InputArgument::REQUIRED,
+                 'Which logfiles should be processed?'
+             );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $filepaths = $input->getArgument('filepaths');
+
+        if (count($filepaths) > 0) {
+            foreach ($filepaths as $fn) {
+                $output->writeln('File: ' . $fn);
+            }
+        } else {
+            throw new \Exception('Error: No logfiles were specified.');
+        }
+
         $output->writeln('It works');
     }
 }

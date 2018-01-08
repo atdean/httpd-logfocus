d55d08f14895e0ad22fd735a935f37313b29f197
Austin Dean <amberdean89@gmail.com>
2018-01-08 03:56:04 -0500

    Refactored FilterByDateRange class to LogfileFilterCommand.

--- src/Commands/LogfileFilterCommand.php
+++ src/Commands/LogfileFilterCommand.php
@@ -14,15 +14,16 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class FilterByDateRange extends Command
+class LogfileFilterCommand extends Command
 {
     protected function configure()
     {
-
+        $this->setName('logfile:filter')
+             ->setDescription('Parses the specified logfile and returns an analysis and/or subset of the data as specified by the given arguments.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-
+        $output->writeln('It works');
     }
 }
